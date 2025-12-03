a99046261968551f6ce2c0f0937ef31198372405
JohnNheil12 <pallaset01@gmail.com>
2025-12-03 18:29:53 +0800

    Add files via upload
    ACTIVITY2

---
+++ MArturillas_/MArturillas_/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MArturillas_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDisplay2 = new System.Windows.Forms.Button();
+            this.btnDisplay1 = new System.Windows.Forms.Button();
+            this.lblMaterial1 = new System.Windows.Forms.Label();
+            this.lblStringNum1 = new System.Windows.Forms.Label();
+            this.lblType1 = new System.Windows.Forms.Label();
+            this.lblSize1 = new System.Windows.Forms.Label();
+            this.lblColor1 = new System.Windows.Forms.Label();
+            this.lblMaterial = new System.Windows.Forms.Label();
+            this.lblStringNum = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::MArturillas_.Properties.Resources._11333;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btnDisplay2);
+            this.panel1.Controls.Add(this.btnDisplay1);
+            this.panel1.Controls.Add(this.lblMaterial1);
+            this.panel1.Controls.Add(this.lblStringNum1);
+            this.panel1.Controls.Add(this.lblType1);
+            this.panel1.Controls.Add(this.lblSize1);
+            this.panel1.Controls.Add(this.lblColor1);
+            this.panel1.Controls.Add(this.lblMaterial);
+            this.panel1.Controls.Add(this.lblStringNum);
+            this.panel1.Controls.Add(this.lblType);
+            this.panel1.Controls.Add(this.lblSize);
+            this.panel1.Controls.Add(this.lblColor);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(433, 466);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnDisplay2
+            // 
+            this.btnDisplay2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay2.Location = new System.Drawing.Point(219, 309);
+            this.btnDisplay2.Name = "btnDisplay2";
+            this.btnDisplay2.Size = new System.Drawing.Size(108, 40);
+            this.btnDisplay2.TabIndex = 12;
+            this.btnDisplay2.Text = "Display Two";
+            this.btnDisplay2.UseVisualStyleBackColor = true;
+            this.btnDisplay2.Click += new System.EventHandler(this.btnDisplay2_Click);
+            // 
+            // btnDisplay1
+            // 
+            this.btnDisplay1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay1.Location = new System.Drawing.Point(59, 309);
+            this.btnDisplay1.Name = "btnDisplay1";
+            this.btnDisplay1.Size = new System.Drawing.Size(108, 40);
+            this.btnDisplay1.TabIndex = 11;
+            this.btnDisplay1.Text = "Display One";
+            this.btnDisplay1.UseVisualStyleBackColor = true;
+            this.btnDisplay1.Click += new System.EventHandler(this.btnDisplay1_Click);
+            // 
+            // lblMaterial1
+            // 
+            this.lblMaterial1.AutoSize = true;
+            this.lblMaterial1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaterial1.Location = new System.Drawing.Point(255, 262);
+            this.lblMaterial1.Name = "lblMaterial1";
+            this.lblMaterial1.Size = new System.Drawing.Size(34, 24);
+            this.lblMaterial1.TabIndex = 10;
+            this.lblMaterial1.Text = "<>";
+            // 
+            // lblStringNum1
+            // 
+            this.lblStringNum1.AutoSize = true;
+            this.lblStringNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStringNum1.Location = new System.Drawing.Point(255, 228);
+            this.lblStringNum1.Name = "lblStringNum1";
+            this.lblStringNum1.Size = new System.Drawing.Size(34, 24);
+            this.lblStringNum1.TabIndex = 9;
+            this.lblStringNum1.Text = "<>";
+            // 
+            // lblType1
+            // 
+            this.lblType1.AutoSize = true;
+            this.lblType1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType1.Location = new System.Drawing.Point(255, 191);
+            this.lblType1.Name = "lblType1";
+            this.lblType1.Size = new System.Drawing.Size(34, 24);
+            this.lblType1.TabIndex = 8;
+            this.lblType1.Text = "<>";
+            // 
+            // lblSize1
+            // 
+            this.lblSize1.AutoSize = true;
+            this.lblSize1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize1.Location = new System.Drawing.Point(255, 152);
+            this.lblSize1.Name = "lblSize1";
+            this.lblSize1.Size = new System.Drawing.Size(34, 24);
+            this.lblSize1.TabIndex = 7;
+            this.lblSize1.Text = "<>";
+            // 
+            // lblColor1
+            // 
+            this.lblColor1.AutoSize = true;
+            this.lblColor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColor1.Location = new System.Drawing.Point(255, 112);
+            this.lblColor1.Name = "lblColor1";
+            this.lblColor1.Size = new System.Drawing.Size(34, 24);
+            this.lblColor1.TabIndex = 6;
+            this.lblColor1.Text = "<>";
+            // 
+            // lblMaterial
+            // 
+            this.lblMaterial.AutoSize = true;
+            this.lblMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaterial.Location = new System.Drawing.Point(52, 262);
+            this.lblMaterial.Name = "lblMaterial";
+            this.lblMaterial.Size = new System.Drawing.Size(89, 24);
+            this.lblMaterial.TabIndex = 5;
+            this.lblMaterial.Text = "Material:";
+            // 
+            // lblStringNum
+            // 
+            this.lblStringNum.AutoSize = true;
+            this.lblStringNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStringNum.Location = new System.Drawing.Point(55, 228);
+            this.lblStringNum.Name = "lblStringNum";
+            this.lblStringNum.Size = new System.Drawing.Size(151, 24);
+            this.lblStringNum.TabIndex = 4;
+            this.lblStringNum.Text = "String Number:";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType.Location = new System.Drawing.Point(55, 191);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(63, 24);
+            this.lblType.TabIndex = 3;
+            this.lblType.Text = "Type:";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize.Location = new System.Drawing.Point(52, 152);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(56, 24);
+            this.lblSize.TabIndex = 2;
+            this.lblSize.Text = "Size:";
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColor.Location = new System.Drawing.Point(52, 112);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(66, 24);
+            this.lblColor.TabIndex = 1;
+            this.lblColor.Text = "Color:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MUSICAL INSTRUMENTS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 461);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblStringNum;
+        private System.Windows.Forms.Label lblMaterial;
+        private System.Windows.Forms.Label lblColor1;
+        private System.Windows.Forms.Label lblSize1;
+        private System.Windows.Forms.Label lblType1;
+        private System.Windows.Forms.Label lblStringNum1;
+        private System.Windows.Forms.Label lblMaterial1;
+        private System.Windows.Forms.Button btnDisplay1;
+        private System.Windows.Forms.Button btnDisplay2;
+    }
+}
+
